d1a205f602a10a782d0c77f7ec56285febb8c10e
Omar.Aceves <lcc.omar.aceves@gmail.com>
2022-04-17 21:29:05 -0700

    method inheritance

--- MozillaGuide/11.1 Inheritance And Prototype chain.js
+++ MozillaGuide/11.1 Inheritance And Prototype chain.js
@@ -31,3 +31,44 @@ myPorsche.displayProtoype(); //{} - Prototype of our myPorsche object is an empt
 myPorsche.displayProtoypeOfPrototype(); //[Object: null prototype] {} - The parent of myPorsche object is an empty object and the parent of this empty object is null,
 //thus representing the end of the prototype inheritance chain. 
 
+//Inheriting properties
+//JavaScript objects are dynamic "bags" of properties. JavaScript objects have a link to a prototype object. 
+//When trying to access a property of an object, the property will not only be sought on the object but on the prototype of the object, 
+//the prototype of the prototype, and so on until either a property with a matching name is found or the end of the prototype chain is reached.
+
+const baseCayman = { engine: 'Boxer Engine'};
+const gtsCayman = { wheels: '20 "', transmission: 'PDK'};
+
+console.log(gtsCayman.engine); //undefined
+
+Object.setPrototypeOf(gtsCayman, baseCayman); //baseCayman is now the base prototype object
+
+console.log(gtsCayman.engine); //Boxer Engine
+
+//Prototypes and ECMAScript standard
+//Since ECMAScript 2015, the [[Prototype]] is accessed using the accessors Object.getPrototypeOf() and Object.setPrototypeOf().
+//This is equivalent to the JavaScript property __proto__ which is non-standard but de-facto implemented by many browsers.
+
+const baseCayman = { engine: 'Boxer Engine'};
+const gtsCayman = { wheels: '20 "', transmission: 'PDK', __proto__: baseCayman};
+
+console.log(gtsCayman.engine); //Boxer Engine
+
+//Inheriting methods
+//JavaScript does not have "methods" in the form that class-based languages define them. 
+//In JavaScript, any function can be added to an object in the form of a property. 
+
+const baseCayman = { 
+    engine: 'Boxer Engine', 
+    displayMake: function () {
+        console.log("Porsche");
+    }};
+const gtsCayman = { wheels: '20 "', transmission: 'PDK'};
+
+Object.setPrototypeOf(gtsCayman, baseCayman); //baseCayman is now the base prototype object
+
+gtsCayman.displayMake(); //Porsche
+
+
+
+
