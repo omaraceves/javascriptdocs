78b4f81b1e9c049c7f3ce44edb7011c80a0a8a18
Omar.Aceves <lcc.omar.aceves@gmail.com>
2022-05-25 22:25:16 -0700

    modules

--- MozillaGuide/17. Javascript Modules.js
+++ MozillaGuide/17. Javascript Modules.js
@@ -3,3 +3,5 @@
 
 //It has made sense in recent years to start thinking about providing mechanisms for splitting 
 //JavaScript programs up into separate modules that can be imported when needed.
+//Use of native JavaScript modules is dependent on the import and export statements;
+
